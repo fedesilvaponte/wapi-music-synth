9cef7a8c608a04ddf9c4512a18e29ba7ffcebe8a
Federico Silva Ponte <federicosilva09@gmail.com>
2016-08-15 20:24:53 -0300

    Update node packages. Add progress bar to build

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -3,26 +3,22 @@ var webpack = require('webpack');
 var path = require('path');
 var _ = require('lodash');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin')
 
 var devConfig = require('./webpack.config.js');
 
 module.exports = _.extend(devConfig, {
     plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'src/index.html'
-        })
+      new ProgressBarPlugin(),
+      new webpack.DefinePlugin({
+          'process.env': {
+              'NODE_ENV': JSON.stringify('production')
+          }
+      }),
+      new HtmlWebpackPlugin({
+          filename: 'index.html',
+          template: 'src/index.html'
+      })
     ],
     devtool: 'source-map',
     entry: './src/index',
