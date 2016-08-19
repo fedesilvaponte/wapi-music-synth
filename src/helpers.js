4088682cc9a34c4a9a926e97d2f2be8ea241d3cc
Federico Silva Ponte <federicosilva09@gmail.com>
2016-08-19 10:24:28 -0300

    fix console error when pressing random keys

--- src/helpers.js
+++ src/helpers.js
@@ -5,11 +5,11 @@
  * @returns {*}
  */
 export function hasClass(el, className) {
-    if (el.classList) {
-        return el.classList.contains(className);
-    } else {
-        return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
-    }
+  if (el.classList) {
+    return el.classList.contains(className);
+  } else {
+    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+  }
 }
 
 /**
@@ -18,11 +18,11 @@ export function hasClass(el, className) {
  * @param className
  */
 export function addClass(el, className) {
-    if (el.classList) {
-        el.classList.add(className);
-    } else if (!hasClass(el, className)) {
-        el.className += ' ' + className;
-    }
+  if (el.classList) {
+    el.classList.add(className);
+  } else if (!hasClass(el, className)) {
+    el.className += ' ' + className;
+  }
 }
 
 /**
@@ -31,12 +31,12 @@ export function addClass(el, className) {
  * @param className
  */
 export function removeClass(el, className) {
-    if (el.classList) {
-        el.classList.remove(className);
-    } else if (hasClass(el, className)) {
-        let reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
-        el.className = el.className.replace(reg, ' ');
-    }
+  if (el.classList) {
+    el.classList.remove(className);
+  } else if (hasClass(el, className)) {
+    let reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+    el.className = el.className.replace(reg, ' ');
+  }
 }
 
 /**
@@ -47,34 +47,37 @@ export function removeClass(el, className) {
  * @returns {*}
  */
 export function getKeyPressed(keyPressed, keys, callback) {
-    let currentKey;
-    switch (keyPressed) {
-        case 'a':
-            currentKey = keys.filter(o => o.name === 'C1' || o.name === 'C4');
-            break;
-        case 'w':
-            currentKey = keys.filter(o => o.name === 'C#1' || o.name === 'C#4');
-            break;
-        case 's':
-            currentKey = keys.filter(o => o.name === 'D1' || o.name === 'D4');
-            break;
-        case 'e':
-            currentKey = keys.filter(o => o.name === 'D#1' || o.name === 'D#4');
-            break;
-        case 'd':
-            currentKey = keys.filter(o => o.name === 'E1' || o.name === 'E4');
-            break;
-        case 'f':
-            currentKey = keys.filter(o => o.name === 'F1' || o.name === 'F4');
-            break;
-        case 't':
-            currentKey = keys.filter(o => o.name === 'F#1' || o.name === 'F#4');
-            break;
-        case 'g':
-            currentKey = keys.filter(o => o.name === 'G1' || o.name === 'G4');
-            break;
-    }
+  let currentKey;
+  switch (keyPressed) {
+    case 'a':
+      currentKey = keys.filter(o => o.name === 'C1' || o.name === 'C4');
+      break;
+    case 'w':
+      currentKey = keys.filter(o => o.name === 'C#1' || o.name === 'C#4');
+      break;
+    case 's':
+      currentKey = keys.filter(o => o.name === 'D1' || o.name === 'D4');
+      break;
+    case 'e':
+      currentKey = keys.filter(o => o.name === 'D#1' || o.name === 'D#4');
+      break;
+    case 'd':
+      currentKey = keys.filter(o => o.name === 'E1' || o.name === 'E4');
+      break;
+    case 'f':
+      currentKey = keys.filter(o => o.name === 'F1' || o.name === 'F4');
+      break;
+    case 't':
+      currentKey = keys.filter(o => o.name === 'F#1' || o.name === 'F#4');
+      break;
+    case 'g':
+      currentKey = keys.filter(o => o.name === 'G1' || o.name === 'G4');
+      break;
+    default:
+      return false;
+      break;
+  }
 
-    return callback(currentKey[0]);
+  return callback(currentKey[0]);
 }
 
